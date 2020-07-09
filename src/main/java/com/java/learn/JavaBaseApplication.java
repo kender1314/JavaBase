29ee5a51fc4cb611550e04e4897652133951dc3e
kender1314 <37035331+kender1314@users.noreply.github.com>
2020-07-09 21:08:50 +0800

    重建项目

---
+++ src/main/java/com/java/learn/JavaBaseApplication.java
@@ -0,0 +1,20 @@
+package com.java.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * JavaBaseApplication
+ * @ClassName JavaBaseApplication
+ * @Description TODO
+ * @Author hejiang
+ * @Date 2020/7/3 20:06
+ */
+@SpringBootApplication
+public class JavaBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBaseApplication.class, args);
+    }
+
+}
