b0b7336b733e9a5324337b97f450d6dd89fa3483
hejiang <18715770814@163.com>
2020-08-05 20:30:21 +0800

    新增原则

---
+++ src/main/java/com/java/learn/design/mode/principle/lod/entity/Fans.java
@@ -0,0 +1,21 @@
+package com.java.learn.design.mode.principle.lod.entity;
+
+/**
+ * 粉丝
+ *
+ * @Author jiang.he
+ * @Version 1.0.0 RELEASE
+ * @Date 2020/7/11 20:12
+ * @Description:
+ */
+public class Fans {
+    private String name;
+
+    public Fans(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/com/java/learn/design/mode/principle/lod/entity/Star.java
@@ -0,0 +1,20 @@
+package com.java.learn.design.mode.principle.lod.entity;
+
+/**
+ * 明星
+ *
+ * @Author jiang.he
+ * @Version 1.0.0 RELEASE
+ * @Date 2020/7/11 20:12
+ * @Description:
+ */
+public class Star {
+    private String name;
+    public Star(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
